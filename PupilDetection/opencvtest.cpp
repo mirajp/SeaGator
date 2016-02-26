90220020ce5cb2f5ba170814cb410334658a1922
cuee <mirage.patel@gmail.com>
2016-02-26 14:47:47 -0500

    Uses version 3

--- PupilDetection/opencvtest.cpp
+++ PupilDetection/opencvtest.cpp
@@ -1,8 +1,12 @@
-#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui.hpp>
 #include <string>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/features2d/features2d.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc.hpp>
+//#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/objdetect.hpp>
+//#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/features2d.hpp>
 #include <iostream>
 #include <cstdio>
 
@@ -11,8 +15,8 @@ using namespace cv;
 int main()
 {
     //char img_str[] = "/home/cuee/justinpng.png";
-    //char img_str[] = "/home/cuee/justinlarger.png";
-    char img_str[] = "/home/cuee/noglassesUP.png";
+    char img_str[] = "/home/cuee/justinlarger.png";
+    //char img_str[] = "/home/cuee/noglassesUP.png";
     Mat img = imread(img_str,CV_LOAD_IMAGE_COLOR);
     Mat img_gray,img_bw;
     cvtColor( img, img_gray, CV_BGR2GRAY );
@@ -50,13 +54,30 @@ int main()
     pupil.filterByInertia = true;
     pupil.minInertiaRatio = 0.01;
 
-    SimpleBlobDetector myBlobDetector(glint);
-    SimpleBlobDetector myBlobDetector2(pupil);
+    //SimpleBlobDetector myBlobDetector2, myBlobDetector;
+    Ptr<SimpleBlobDetector> myBlobDetector2, myBlobDetector;
+    //SimpleBlobDetector myBlobDetector(glint);
+    //SimpleBlobDetector myBlobDetector();
+    myBlobDetector = SimpleBlobDetector::create(glint);
+    // SimpleBlobDetector::create creates a smart pointer. 
+    // So you need to use arrow ( ->) instead of dot ( . )
+    // detector->detect( im, keypoints);
+
+    //SimpleBlobDetector myBlobDetector2(pupil);
+    //SimpleBlobDetector myBlobDetector2();
+    //myBlobDetector2 = cv2.SimpleBlobDetector_create(pupil);
+    myBlobDetector2 = SimpleBlobDetector::create(pupil);
+
     std::vector<cv::KeyPoint> myBlobs;
     std::vector<cv::KeyPoint> myBlobs2;
     //inRange(img, Scalar(158, 158, 158), Scalar(255, 255, 255), img);
-    myBlobDetector.detect(img_gray, myBlobs);
-    myBlobDetector2.detect(img_gray,myBlobs2);
+    // Ver2
+    //myBlobDetector.detect(img_gray, myBlobs);
+    //myBlobDetector2.detect(img_gray,myBlobs2);
+    // Ver 3
+    myBlobDetector->detect(img_gray, myBlobs);
+    myBlobDetector2->detect(img_gray,myBlobs2);
+
     //imshow("opencvtest",img);
     
     //myBlobDetector.detect(img, myBlobs);
--- PupilDetection/videocam.cpp
+++ PupilDetection/videocam.cpp
@@ -1,26 +1,107 @@
 #include "opencv2/opencv.hpp"
+#include <sys/time.h>
 
 using namespace cv;
+
+cv::Mat processImage(Mat src) {
+    cv::Mat gray;
+    cvtColor(src, gray, CV_BGR2GRAY);
+
+    //equalizeHist(img_gray, img_gray);
+    cv::SimpleBlobDetector::Params glint; 
+    glint.minDistBetweenBlobs = 1;  // minimum 10 pixels between blobs
+    glint.filterByArea = true;         // filter blobs by area of blob
+    glint.minArea = 10;              // min 20 pixels squared
+    glint.maxArea = 15;             // max 500 pixels squared
+    glint.filterByColor = true;
+    glint.blobColor = 255;
+
+    glint.filterByConvexity = false;
+
+    glint.filterByCircularity = false;
+    glint.minCircularity = 0.01;
+
+    glint.filterByInertia = true;
+    glint.minInertiaRatio = 0.01;
+
+    cv::SimpleBlobDetector::Params pupil;
+    pupil.minDistBetweenBlobs = 1;  // minimum 10 pixels between blobs
+    pupil.filterByArea = true;         // filter blobs by area of blob
+    pupil.minArea = 100;              // min 20 pixels squared
+    pupil.maxArea = 200.0;             // max 500 pixels squared
+    pupil.filterByColor = true;
+    pupil.blobColor = 0;
+
+    pupil.filterByConvexity = false;
+
+    pupil.filterByCircularity = true;
+    pupil.minCircularity = 0.01;
+
+    pupil.filterByInertia = true;
+    pupil.minInertiaRatio = 0.01;
+
+
+    //Ver 2
+    //SimpleBlobDetector glintDetector(glint);
+    //SimpleBlobDetector pupilDetector(pupil);
+
+    //Ver 3
+    Ptr<SimpleBlobDetector> glintDetector = SimpleBlobDetector::create(glint);
+    Ptr<SimpleBlobDetector> pupilDetector = SimpleBlobDetector::create(pupil);
+    
+    std::vector<cv::KeyPoint> myGlints;
+    std::vector<cv::KeyPoint> myPupils;
+    //inRange(img, Scalar(158, 158, 158), Scalar(255, 255, 255), img);
+    //Ver2
+    //glintDetector.detect(gray, myGlints);
+    //pupilDetector.detect(gray,myPupils);
+    //Ver3
+    glintDetector->detect(gray, myGlints);
+    pupilDetector->detect(gray,myPupils);
+    //imshow("opencvtest",img);
+        
+    //glintDetector.detect(img, myGlints);
+    cv::Mat blobImg,blobImg2;    
+    //cv::drawKeypoints(img_gray, myGlints, blobImg, 1);
+    cv::drawKeypoints(src, myGlints, blobImg, Scalar(0, 0, 255), 4); // make the glints blue
+    cv::drawKeypoints(blobImg,myPupils,blobImg2, Scalar(0, 255, 0), 4); // make the pupils green
+
+
+    //imshow("Video", img);
+        
+   // if(waitKey(30) >= 0)
+     //   break;
+
+    return blobImg2;
+
+}
 
 int main(int, char**)
 {
     VideoCapture cap(0); // open the default camera
+    //VideoCapture cap2(1); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
+    struct timeval t1, t2;
+    int numFrames = 0;
+    gettimeofday(&t1, NULL);
     Mat img_gray;
-    namedWindow("Video",1);
     for(;;)
     {
-        Mat frame;
+        numFrames++;
+        Mat frame, frame2;
         cap >> frame; // get a new frame from camera
+        //cap2 >> frame2;
+        /*
         cvtColor(frame, img_gray, CV_BGR2GRAY);
-        
+
+        //equalizeHist(img_gray, img_gray);
         cv::SimpleBlobDetector::Params glint; 
         glint.minDistBetweenBlobs = 1;  // minimum 10 pixels between blobs
         glint.filterByArea = true;         // filter blobs by area of blob
-        glint.minArea = 0.1;              // min 20 pixels squared
-        glint.maxArea = 20.0;             // max 500 pixels squared
+        glint.minArea = 10;              // min 20 pixels squared
+        glint.maxArea = 15;             // max 500 pixels squared
         glint.filterByColor = true;
         glint.blobColor = 255;
 
@@ -35,8 +116,8 @@ int main(int, char**)
         cv::SimpleBlobDetector::Params pupil;
         pupil.minDistBetweenBlobs = 1;  // minimum 10 pixels between blobs
         pupil.filterByArea = true;         // filter blobs by area of blob
-        pupil.minArea = 10;              // min 20 pixels squared
-        pupil.maxArea = 1000.0;             // max 500 pixels squared
+        pupil.minArea = 100;              // min 20 pixels squared
+        pupil.maxArea = 200.0;             // max 500 pixels squared
         pupil.filterByColor = true;
         pupil.blobColor = 0;
 
@@ -48,26 +129,36 @@ int main(int, char**)
         pupil.filterByInertia = true;
         pupil.minInertiaRatio = 0.01;
 
-        SimpleBlobDetector myBlobDetector(glint);
-        SimpleBlobDetector myBlobDetector2(pupil);
-        std::vector<cv::KeyPoint> myBlobs;
-        std::vector<cv::KeyPoint> myBlobs2;
+        SimpleBlobDetector glintDetector(glint);
+        SimpleBlobDetector pupilDetector(pupil);
+        std::vector<cv::KeyPoint> myGlints;
+        std::vector<cv::KeyPoint> myPupils;
         //inRange(img, Scalar(158, 158, 158), Scalar(255, 255, 255), img);
-        myBlobDetector.detect(img_gray, myBlobs);
-        myBlobDetector2.detect(img_gray,myBlobs2);
+        glintDetector.detect(img_gray, myGlints);
+        pupilDetector.detect(img_gray,myPupils);
         //imshow("opencvtest",img);
         
-        //myBlobDetector.detect(img, myBlobs);
+        //glintDetector.detect(img, myGlints);
         cv::Mat blobImg,blobImg2;    
-        cv::drawKeypoints(img_gray, myBlobs, blobImg, 1);
-        cv::drawKeypoints(blobImg,myBlobs2,blobImg2,1);
-        cv::imshow("Blobs", blobImg2);
+        //cv::drawKeypoints(img_gray, myGlints, blobImg, 1);
+        cv::drawKeypoints(frame, myGlints, blobImg, Scalar(0, 0, 255), 4); // make the glints blue
+        cv::drawKeypoints(blobImg,myPupils,blobImg2, Scalar(0, 255, 0), 4); // make the pupils green
         
-
-
+        */
+        cv::Mat blobImg2 = processImage(frame);
+        //cv::Mat fresh2 = processImage(frame2);
+        cv::imshow("Blobs", blobImg2);
+        //cv::imshow("Fresh", fresh2);
+        gettimeofday(&t2, NULL);
+        int microSeconds = (t2.tv_usec - t1.tv_usec);
+        int milliSeconds = (t2.tv_sec - t1.tv_sec)*1000;
+        int timeDiff = microSeconds/1000 + milliSeconds;
+        int fps = numFrames/(timeDiff/1000);
+        //std::cout << "Time diff (ms) = " << milliSeconds << std::endl;
+        std::cout << "FPS = " << fps << std::endl;
         //imshow("Video", img);
         
-        if(waitKey(30) >= 0)
+        if(waitKey(30) == 'e')
             break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
